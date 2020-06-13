6f0661a36ddb8e513e0fa9e0f29265cba79bdb3f
mishaQ23n <65655909+mishaQ23n@users.noreply.github.com>
2020-06-13 19:37:13 +0300

    Update Naktutka.py

--- Naktutka.py
+++ Naktutka.py
@@ -11,11 +11,15 @@ vibor = input("-->")
 chelo = input("Введите ID первого человека который будет в беседе:")
 chels = input("Введите ID первого человека который будет в беседе:")
 tokene = input("Введите токен от кого будет создаваться беседа")	
+kalas = input("Кол-во смс: ")
 vk_session = vk_api.VkApi(token=tokene)
 vk = vk_session.get_api()
 chelii = [chelo,chels]
+sdss = 0
+while int(sdss) < int(kalas):
 try:
 	vk.messages.createChat(user_ids=chelii,title='pizdatya nakrutka chmotie')
+	sdss += 1
 	time.sleep(60)
 except vk_api.exceptions.ApiError:
    print("Чет не так Error 444")
